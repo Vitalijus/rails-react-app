4d19e4d801ba674ab5a3c7885c76c92314783ebb
Vitalijus Desukas <vitalij.desuk@gmail.com>
2019-10-13 21:33:51 +0100

    try out inline styles, because heroku does not render styles as component

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react'
-// import Logo from 'https://res.cloudinary.com/dhgsrpwf7/image/upload/v1570987839/logo_2x_uarhkg.png'
 
 class Header extends Component {
 
 render() {
+  const header = {
+    width: '100%',
+    background: '#051830',
+    height: '60px',
+    display: 'table',
+  };
+
     return(
-      <header>
+      <header style={header}>
         <div className="logo">
           <img src="https://res.cloudinary.com/dhgsrpwf7/image/upload/v1570987839/logo_2x_uarhkg.png" width={"100"} alt={"website logo"} />
         </div>
